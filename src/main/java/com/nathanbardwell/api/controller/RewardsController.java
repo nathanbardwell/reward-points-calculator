eaad3350f4e793c5bf3e4f5696f16e59e6ffd65e
Nathan Bardwell <nathan.bardwell@champtitles.com>
2022-09-26 01:11:02 -0400

    Implemented RewardsService

--- src/main/java/com/nathanbardwell/api/controller/RewardsController.java
+++ src/main/java/com/nathanbardwell/api/controller/RewardsController.java
@@ -6,6 +6,7 @@ import com.nathanbardwell.api.service.RewardsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class RewardsController {
 	}
 
 	@GetMapping("/calculate-points")
-	public ResponseEntity<RewardPointsResponse> calculatePoints(TransactionRecordsRequest request) {
+	public ResponseEntity<RewardPointsResponse> calculatePoints(@RequestBody TransactionRecordsRequest request) {
 		RewardPointsResponse response = rewardsService.calculateRewardsPoints(request);
 
 		return ResponseEntity.ok(response);
--- src/main/java/com/nathanbardwell/api/service/RewardsService.java
+++ src/main/java/com/nathanbardwell/api/service/RewardsService.java
@@ -1,13 +1,87 @@
 package com.nathanbardwell.api.service;
 
+import com.nathanbardwell.api.dto.CustomerRewardPoints;
 import com.nathanbardwell.api.dto.RewardPointsResponse;
+import com.nathanbardwell.api.dto.TransactionRecord;
 import com.nathanbardwell.api.dto.TransactionRecordsRequest;
 import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class RewardsService {
 
 	public RewardPointsResponse calculateRewardsPoints(TransactionRecordsRequest request) {
-		return new RewardPointsResponse();
+		Map<String, List<TransactionRecord>> customerTransactions = organizeTransactionsByCustomerId(request.getTransactions());
+
+		List<CustomerRewardPoints> customerRewardPoints = customerTransactions.entrySet().stream()
+				.map(entry -> calculateCustomerRewardPoints(entry.getKey(), entry.getValue()))
+				.toList();
+
+		return new RewardPointsResponse(customerRewardPoints);
+	}
+
+	private Map<String, List<TransactionRecord>> organizeTransactionsByCustomerId(List<TransactionRecord> transactions) {
+		HashMap<String, List<TransactionRecord>> customerTransactions = new HashMap<>();
+
+		for (TransactionRecord transaction : transactions) {
+			String customerId = transaction.getCustomerId();
+
+			if (customerTransactions.containsKey(customerId)) {
+				customerTransactions.get(customerId).add(transaction);
+			} else {
+				List<TransactionRecord> transactionRecords = new ArrayList<>();
+				transactionRecords.add(transaction);
+				customerTransactions.put(customerId, transactionRecords);
+			}
+		}
+
+		return customerTransactions;
+	}
+
+	private CustomerRewardPoints calculateCustomerRewardPoints(String customerId, List<TransactionRecord> transactions) {
+		Map<String, Integer> monthlyRewardPoints = calculateMonthlyRewardPoints(transactions);
+		int totalPoints = monthlyRewardPoints.values().stream().mapToInt(i -> i).sum();
+
+		return new CustomerRewardPoints(customerId, totalPoints, monthlyRewardPoints);
+	}
+
+	private Map<String, Integer> calculateMonthlyRewardPoints(List<TransactionRecord> transactions) {
+		Map<String, Integer> monthlyRewardPoints = new HashMap<>();
+
+		for (TransactionRecord transaction : transactions) {
+			String transactionMonthYear = transaction.getPurchaseDate().format(DateTimeFormatter.ofPattern("LLL yyyy"));
+			int transactionPointsEarned = calculatePointsForPurchaseAmount(transaction.getPurchaseAmount());
+
+			if (monthlyRewardPoints.containsKey(transactionMonthYear)) {
+				int newPointsTotal = monthlyRewardPoints.get(transactionMonthYear) + transactionPointsEarned;
+				monthlyRewardPoints.put(transactionMonthYear, newPointsTotal);
+			} else {
+				monthlyRewardPoints.put(transactionMonthYear, transactionPointsEarned);
+			}
+		}
+
+		return monthlyRewardPoints;
+	}
+
+	private int calculatePointsForPurchaseAmount(BigDecimal purchaseAmount){
+		int purchaseDollarAmount = purchaseAmount.intValue();
+		int totalPoints = 0;
+
+		if (purchaseDollarAmount > 50) {
+			int dollarsOver50 = purchaseDollarAmount - 50;
+			totalPoints += dollarsOver50;
+		}
+		if (purchaseDollarAmount > 100) {
+			int dollarsOver100 = purchaseDollarAmount - 100;
+			totalPoints += dollarsOver100;
+		}
+
+		return totalPoints;
 	}
 }
