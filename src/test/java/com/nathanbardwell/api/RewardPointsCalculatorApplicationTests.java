278a7bcbbecaf237dc954f824d4d80ddb9d04ba5
Nathan Bardwell <nathanbardwell@github.com>
2023-06-08 15:58:14 -0400

    Initial commit

---
+++ src/test/java/com/nathanbardwell/api/RewardPointsCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nathanbardwell.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RewardPointsCalculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
